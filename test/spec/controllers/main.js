73fe4225b54b305385bdcc8478849e4967d72913
Aaron Lieberman <aaron@bigcompass.com>
2016-02-04 14:04:45 -0700

    first commit

---
+++ test/spec/controllers/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('Controller: MainCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('karmaApp'));
+
+  var MainCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(MainCtrl.awesomeThings.length).toBe(3);
+  });
+  it('array contains AngularJS', function () {
+    expect(MainCtrl.awesomeThings).toEqual(jasmine.arrayContaining(['AngularJS']));
+  });
+});
